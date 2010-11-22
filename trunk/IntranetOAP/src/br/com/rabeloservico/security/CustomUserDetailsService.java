7edbb2ab08ce77e61f27bf5fd4d9574f14be6820
fmrabelo <fmrabelo@38c0dbe2-1d61-b02a-7774-c78bf3521c0c>
2010-11-22 00:11:06 +0000

    configuração spring security.

---
+++ trunk/IntranetOAP/src/br/com/rabeloservico/security/CustomUserDetailsService.java
@@ -0,0 +1,45 @@
+package br.com.rabeloservico.security;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.security.GrantedAuthority;
+import org.springframework.security.userdetails.UserDetails;
+import org.springframework.security.userdetails.jdbc.JdbcDaoImpl;
+
+public class CustomUserDetailsService
+    extends JdbcDaoImpl
+{
+
+	@Override
+	protected UserDetails createUserDetails (String username, UserDetails userFromUserQuery,
+	    GrantedAuthority[] combinedAuthorities)
+	{
+		Usuario usuario = new Usuario();
+
+		usuario.setLogin(userFromUserQuery.getUsername());
+		usuario.setSenha(userFromUserQuery.getPassword());
+		usuario.setAtivo(userFromUserQuery.isEnabled());
+		usuario.setAuthorities(combinedAuthorities);
+
+		this.carregarInformacoesAdicionais(usuario);
+
+		return usuario;
+	}
+
+	private void carregarInformacoesAdicionais (final Usuario usuario)
+	{
+		String sql = "select email, tentativas_login from Usuario where login = ?";
+
+		getJdbcTemplate().query(sql, new Object[]{usuario.getUsername()}, new RowMapper() {
+			public Object mapRow (ResultSet rs, int rowNum) throws SQLException
+			{
+				usuario.setEmail(rs.getString("email"));
+				usuario.setTentativasLogin(rs.getInt("tentativas_login"));
+				return null;
+			}
+		});
+	}
+
+}
