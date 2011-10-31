a90fca1e3b4c05fd6563bcddd748dfbac0abb279
fmrabelo <fmrabelo@38c0dbe2-1d61-b02a-7774-c78bf3521c0c>
2011-10-31 23:08:15 +0000

    Implementação de classes utilitarias e DAO para conexao com base de dados via DataSource Tomcat (server.xml)

--- trunk/IntranetOAP/src/br/com/oappr/intranet/vo/LaudoVO.java
+++ trunk/IntranetOAP/src/br/com/oappr/intranet/vo/LaudoVO.java
@@ -5,6 +5,8 @@ package br.com.oappr.intranet.vo;
 
 import java.util.Date;
 
+import oracle.sql.BLOB;
+
 /**
  * @author rabelo
  */
@@ -17,10 +19,27 @@ public final class LaudoVO
 	 */
 	private static final long serialVersionUID = 10216598875464987L;
 
-	private Long id;
-	private String descricao;
-	private boolean finalizado;
-	private Date dataFinalizacao;
+	// private Long id;
+	// private String descricao;
+	// private boolean finalizado;
+	// private Date dataFinalizacao;
+	//	
+	private Long nrrequisicao;
+	private Long nrseqresultado;
+	private Date dtconsulta;
+	private Date hragenda;
+	private Long nrusuarioamb;
+	private Long cdpessoa;
+	private Long cdproced;
+	private String dsexamecompl;
+	private Long nrlaudo;
+	private Long nrusuarioinc;
+	private Date dhinclusao;
+	private Long nrusuarioalt;
+	private Date dhalteracao;
+	private Long cdconvenio;
+	private Long nrususolic;
+	private BLOB dsrtf;
 
 	/**
 	 * 
@@ -37,72 +56,333 @@ public final class LaudoVO
 	public LaudoVO (Long id, String descricao)
 	{
 		super();
-		this.id = id;
-		this.descricao = descricao;
 	}
 
 	/**
-	 * @return the id
+	 * @return the nrrequisicao
 	 */
-	public Long getId ()
+	public Long getNrrequisicao ()
 	{
-		return id;
+		return nrrequisicao;
 	}
 
 	/**
-	 * @param id the id to set
+	 * @param nrrequisicao the nrrequisicao to set
 	 */
-	public void setId (Long id)
+	public void setNrrequisicao (Long nrrequisicao)
 	{
-		this.id = id;
+		this.nrrequisicao = nrrequisicao;
 	}
 
 	/**
-	 * @return the descricao
+	 * @return the nrseqresultado
 	 */
-	public String getDescricao ()
+	public Long getNrseqresultado ()
 	{
-		return descricao;
+		return nrseqresultado;
 	}
 
 	/**
-	 * @param descricao the descricao to set
+	 * @param nrseqresultado the nrseqresultado to set
 	 */
-	public void setDescricao (String descricao)
+	public void setNrseqresultado (Long nrseqresultado)
 	{
-		this.descricao = descricao;
+		this.nrseqresultado = nrseqresultado;
 	}
 
 	/**
-	 * @return the finalizado
+	 * @return the dtconsulta
 	 */
-	public boolean isFinalizado ()
+	public Date getDtconsulta ()
 	{
-		return finalizado;
+		return dtconsulta;
 	}
 
 	/**
-	 * @param finalizado the finalizado to set
+	 * @param dtconsulta the dtconsulta to set
 	 */
-	public void setFinalizado (boolean finalizado)
+	public void setDtconsulta (Date dtconsulta)
 	{
-		this.finalizado = finalizado;
+		this.dtconsulta = dtconsulta;
 	}
 
 	/**
-	 * @return the dataFinalizacao
+	 * @return the hragenda
 	 */
-	public Date getDataFinalizacao ()
+	public Date getHragenda ()
 	{
-		return dataFinalizacao;
+		return hragenda;
 	}
 
 	/**
-	 * @param dataFinalizacao the dataFinalizacao to set
+	 * @param hragenda the hragenda to set
 	 */
-	public void setDataFinalizacao (Date dataFinalizacao)
+	public void setHragenda (Date hragenda)
 	{
-		this.dataFinalizacao = dataFinalizacao;
+		this.hragenda = hragenda;
+	}
+
+	/**
+	 * @return the nrusuarioamb
+	 */
+	public Long getNrusuarioamb ()
+	{
+		return nrusuarioamb;
+	}
+
+	/**
+	 * @param nrusuarioamb the nrusuarioamb to set
+	 */
+	public void setNrusuarioamb (Long nrusuarioamb)
+	{
+		this.nrusuarioamb = nrusuarioamb;
+	}
+
+	/**
+	 * @return the cdpessoa
+	 */
+	public Long getCdpessoa ()
+	{
+		return cdpessoa;
+	}
+
+	/**
+	 * @param cdpessoa the cdpessoa to set
+	 */
+	public void setCdpessoa (Long cdpessoa)
+	{
+		this.cdpessoa = cdpessoa;
+	}
+
+	/**
+	 * @return the cdproced
+	 */
+	public Long getCdproced ()
+	{
+		return cdproced;
+	}
+
+	/**
+	 * @param cdproced the cdproced to set
+	 */
+	public void setCdproced (Long cdproced)
+	{
+		this.cdproced = cdproced;
+	}
+
+	/**
+	 * @return the dsexamecompl
+	 */
+	public String getDsexamecompl ()
+	{
+		return dsexamecompl;
+	}
+
+	/**
+	 * @param dsexamecompl the dsexamecompl to set
+	 */
+	public void setDsexamecompl (String dsexamecompl)
+	{
+		this.dsexamecompl = dsexamecompl;
+	}
+
+	/**
+	 * @return the nrlaudo
+	 */
+	public Long getNrlaudo ()
+	{
+		return nrlaudo;
+	}
+
+	/**
+	 * @param nrlaudo the nrlaudo to set
+	 */
+	public void setNrlaudo (Long nrlaudo)
+	{
+		this.nrlaudo = nrlaudo;
+	}
+
+	/**
+	 * @return the nrusuarioinc
+	 */
+	public Long getNrusuarioinc ()
+	{
+		return nrusuarioinc;
+	}
+
+	/**
+	 * @param nrusuarioinc the nrusuarioinc to set
+	 */
+	public void setNrusuarioinc (Long nrusuarioinc)
+	{
+		this.nrusuarioinc = nrusuarioinc;
+	}
+
+	/**
+	 * @return the dhinclusao
+	 */
+	public Date getDhinclusao ()
+	{
+		return dhinclusao;
+	}
+
+	/**
+	 * @param dhinclusao the dhinclusao to set
+	 */
+	public void setDhinclusao (Date dhinclusao)
+	{
+		this.dhinclusao = dhinclusao;
+	}
+
+	/**
+	 * @return the nrusuarioalt
+	 */
+	public Long getNrusuarioalt ()
+	{
+		return nrusuarioalt;
+	}
+
+	/**
+	 * @param nrusuarioalt the nrusuarioalt to set
+	 */
+	public void setNrusuarioalt (Long nrusuarioalt)
+	{
+		this.nrusuarioalt = nrusuarioalt;
+	}
+
+	/**
+	 * @return the dhalteracao
+	 */
+	public Date getDhalteracao ()
+	{
+		return dhalteracao;
+	}
+
+	/**
+	 * @param dhalteracao the dhalteracao to set
+	 */
+	public void setDhalteracao (Date dhalteracao)
+	{
+		this.dhalteracao = dhalteracao;
+	}
+
+	/**
+	 * @return the cdconvenio
+	 */
+	public Long getCdconvenio ()
+	{
+		return cdconvenio;
+	}
+
+	/**
+	 * @param cdconvenio the cdconvenio to set
+	 */
+	public void setCdconvenio (Long cdconvenio)
+	{
+		this.cdconvenio = cdconvenio;
+	}
+
+	/**
+	 * @return the nrususolic
+	 */
+	public Long getNrususolic ()
+	{
+		return nrususolic;
+	}
+
+	/**
+	 * @param nrususolic the nrususolic to set
+	 */
+	public void setNrususolic (Long nrususolic)
+	{
+		this.nrususolic = nrususolic;
+	}
+
+	/**
+	 * @return the dsrtf
+	 */
+	public BLOB getDsrtf ()
+	{
+		return dsrtf;
+	}
+
+	/**
+	 * @param dsrtf the dsrtf to set
+	 */
+	public void setDsrtf (BLOB dsrtf)
+	{
+		this.dsrtf = dsrtf;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode ()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cdconvenio == null) ? 0 : cdconvenio.hashCode());
+		result = prime * result + ((cdpessoa == null) ? 0 : cdpessoa.hashCode());
+		result = prime * result + ((cdproced == null) ? 0 : cdproced.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals (Object obj)
+	{
+		if (this == obj)
+		{
+			return true;
+		}
+		if (obj == null)
+		{
+			return false;
+		}
+		if (this.getClass() != obj.getClass())
+		{
+			return false;
+		}
+		final LaudoVO other = (LaudoVO)obj;
+		if (cdconvenio == null)
+		{
+			if (other.cdconvenio != null)
+			{
+				return false;
+			}
+		}
+		else if (!cdconvenio.equals(other.cdconvenio))
+		{
+			return false;
+		}
+		if (cdpessoa == null)
+		{
+			if (other.cdpessoa != null)
+			{
+				return false;
+			}
+		}
+		else if (!cdpessoa.equals(other.cdpessoa))
+		{
+			return false;
+		}
+		if (cdproced == null)
+		{
+			if (other.cdproced != null)
+			{
+				return false;
+			}
+		}
+		else if (!cdproced.equals(other.cdproced))
+		{
+			return false;
+		}
+		return true;
 	}
 
 }
---
+++ trunk/IntranetOAP/src/br/com/oappr/intranet/vo/PessoaVO.java
@@ -0,0 +1,167 @@
+/**
+ * 
+ */
+package br.com.oappr.intranet.vo;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author rabelo
+ */
+public final class PessoaVO
+    implements java.io.Serializable
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 10216598811338787L;
+
+	private Long cdPessoa;
+	private String nomePessoa;
+	private Date dataNascimento;
+
+	private List<LaudoVO> listaLaudos;
+
+	/**
+	 * @return the listaLaudos
+	 */
+	public List<LaudoVO> getListaLaudos ()
+	{
+		return listaLaudos;
+	}
+
+	/**
+	 * @param listaLaudos the listaLaudos to set
+	 */
+	public void setListaLaudos (List<LaudoVO> listaLaudos)
+	{
+		this.listaLaudos = listaLaudos;
+	}
+
+	/**
+	 * 
+	 */
+	public PessoaVO ()
+	{
+		super();
+	}
+
+	/**
+	 * @param id
+	 * @param descricao
+	 */
+	public PessoaVO (Long id, String descricao)
+	{
+		super();
+	}
+
+	/**
+	 * @return the cdPessoa
+	 */
+	public Long getCdPessoa ()
+	{
+		return cdPessoa;
+	}
+
+	/**
+	 * @param cdPessoa the cdPessoa to set
+	 */
+	public void setCdPessoa (Long cdPessoa)
+	{
+		this.cdPessoa = cdPessoa;
+	}
+
+	/**
+	 * @return the nomePessoa
+	 */
+	public String getNomePessoa ()
+	{
+		return nomePessoa;
+	}
+
+	/**
+	 * @param nomePessoa the nomePessoa to set
+	 */
+	public void setNomePessoa (String nomePessoa)
+	{
+		this.nomePessoa = nomePessoa;
+	}
+
+	/**
+	 * @return the dataNascimento
+	 */
+	public Date getDataNascimento ()
+	{
+		return dataNascimento;
+	}
+
+	/**
+	 * @param dataNascimento the dataNascimento to set
+	 */
+	public void setDataNascimento (Date dataNascimento)
+	{
+		this.dataNascimento = dataNascimento;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode ()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cdPessoa == null) ? 0 : cdPessoa.hashCode());
+		result = prime * result + ((dataNascimento == null) ? 0 : dataNascimento.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals (Object obj)
+	{
+		if (this == obj)
+		{
+			return true;
+		}
+		if (obj == null)
+		{
+			return false;
+		}
+		if (this.getClass() != obj.getClass())
+		{
+			return false;
+		}
+		final PessoaVO other = (PessoaVO)obj;
+		if (cdPessoa == null)
+		{
+			if (other.cdPessoa != null)
+			{
+				return false;
+			}
+		}
+		else if (!cdPessoa.equals(other.cdPessoa))
+		{
+			return false;
+		}
+		if (dataNascimento == null)
+		{
+			if (other.dataNascimento != null)
+			{
+				return false;
+			}
+		}
+		else if (!dataNascimento.equals(other.dataNascimento))
+		{
+			return false;
+		}
+		return true;
+	}
+
+}
