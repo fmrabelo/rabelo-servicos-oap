1ec9c9eee50df8070e6dce910ad3d6ca82bc93ce
fmrabelo <fmrabelo@38c0dbe2-1d61-b02a-7774-c78bf3521c0c>
2011-10-31 23:08:32 +0000

    Implementação de classes utilitarias e DAO para conexao com base de dados via DataSource Tomcat (server.xml)

--- trunk/IntranetOAP/src/br/com/oappr/infra/action/AutenticacaoLaudoOnlineAction.java
+++ trunk/IntranetOAP/src/br/com/oappr/infra/action/AutenticacaoLaudoOnlineAction.java
@@ -3,20 +3,19 @@
  */
 package br.com.oappr.infra.action;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Result;
 
+import br.com.oappr.infra.DAO.DaoFactory;
+import br.com.oappr.infra.util.DateUtils;
 import br.com.oappr.intranet.vo.LaudoVO;
+import br.com.oappr.intranet.vo.PessoaVO;
 
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -35,7 +34,7 @@ public class AutenticacaoLaudoOnlineAction
 	 */
 	private static final long serialVersionUID = 13102010197645454L;
 
-	private List<LaudoVO> listaLaudos;
+	private PessoaVO pessoaVo;
 
 	// parametros para jasper report
 	HashMap<String, String> parameters = new HashMap<String, String>();
@@ -71,19 +70,30 @@ public class AutenticacaoLaudoOnlineAction
 	}
 
 	@Action(value = "listarLaudos", results = {@Result(location = "/jsp/listaLaudos.jsp", name = "success")})
-	public String listarLaudos ()
+	public String listarLaudos () throws Exception
 	{
-		System.out.println("listar laudos...");
 		// pesquisar laudos
-
-		this.setListaLaudos(this.getLaudos());
-		this.setDataNascimento(Calendar.getInstance().getTime());
-		if ((listaLaudos != null) && !listaLaudos.isEmpty())
+		try
 		{
-			for (LaudoVO v : listaLaudos)
+			this.setListaLaudos(DaoFactory.getInstance().getLaudos(this.getNroCadastroPaciente()));
+			System.out.println("listar laudos. Quantidade-> "
+			    + (this.getListaLaudos() != null ? this.getListaLaudos().size() : 0));
+			if ((this.getListaLaudos() != null) && !this.getListaLaudos().isEmpty())
 			{
-				System.out.printf("ID: %s Laudo: %s %n", v.getId(), v.getDescricao());
+				for (LaudoVO v : this.getListaLaudos())
+				{
+					System.out.printf("ID: %s Laudo: %s %n", v.getNrlaudo(), v.getDsexamecompl());
+				}
 			}
+			else
+			{
+				this.addFieldError("nroCadastroPaciente", "Nenhum registro de Laudo localizado!!");
+				return com.opensymphony.xwork2.Action.ERROR;
+			}
+		}
+		catch (Exception ex)
+		{
+			throw new Exception(ex);
 		}
 		return com.opensymphony.xwork2.Action.SUCCESS;
 	}
@@ -96,58 +106,58 @@ public class AutenticacaoLaudoOnlineAction
 	{
 		System.out.println("PDF laudos...");
 
-		HttpServletRequest request = ServletActionContext.getRequest();
-		String imgPath = "";
+		// HttpServletRequest request = ServletActionContext.getRequest();
+		// String imgPath = "";
+		//
+		// if (request != null)
+		// {
+		// imgPath = request.getSession().getServletContext().getRealPath(
+		// new
+		// StringBuilder(File.separator).append("logo").append(File.separator).append(
+		// "logo2.jpg").toString());
+		// }
+		// String id = request.getParameter("id");
 
-		if (request != null)
-		{
-			imgPath = request.getSession().getServletContext().getRealPath(
-			    new StringBuilder(File.separator).append("logo").append(File.separator).append(
-			        "logo2.jpg").toString());
-		}
-		final String paciente = "MARIA DA CONCEIÇÃO CRISANTO MALLIN";
-		final String medico = "DRA MARIA ISABEL BORA CASTALDO ANDRADE";
-		String laudo = "LAUDO TESTE.....";
-
-		String id = request.getParameter("id");
 		// recuperar laudo pelo id
-		this.setListaLaudos(this.getLaudos());
-		if ((listaLaudos != null) && !listaLaudos.isEmpty())
-		{
-			for (final LaudoVO v : listaLaudos)
-			{
-				if (v.getId().longValue() == new Long(id).longValue())
-				{
-					laudo = v.getDescricao();
-					break;
-				}
-			}
-		}
 
-		parameters.put("LOGO_PATH", imgPath);
-		parameters.put("PACIENTE", "PACIENTE : " + paciente);
-		parameters.put("DESCR_MEDICO", "A/C : " + medico);
-		parameters.put("DESCR_LAUDO", laudo);
-
-		this.setParameters(parameters);
-		try
-		{
-			if ((listaLaudos != null) && !listaLaudos.isEmpty())
-			{
-				System.out.println("Lista Laudos OK. " + listaLaudos.size());
-			}
-			else
-			{
-				this.listarLaudos();
-			}
-			// JasperCompileManager.compileReportToFile("/reports/TONOMETRIA.jrxml",
-			// "/reports/TONOMETRIA.jasper");
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-			return com.opensymphony.xwork2.Action.ERROR;
-		}
+		// this.setListaLaudos(this.getLaudos());
+		// if ((listaLaudos != null) && !listaLaudos.isEmpty())
+		// {
+		// for (final LaudoVO v : listaLaudos)
+		// {
+		// if (v.getCdpessoa().longValue() == new Long(id).longValue())
+		// {
+		// laudo = v.getDescricao();
+		// break;
+		// }
+		// }
+		// }
+		//
+		// parameters.put("LOGO_PATH", imgPath);
+		// parameters.put("PACIENTE", "PACIENTE : " + paciente);
+		// parameters.put("DESCR_MEDICO", "A/C : " + medico);
+		// parameters.put("DESCR_LAUDO", laudo);
+		//
+		// this.setParameters(parameters);
+		// try
+		// {
+		// if ((listaLaudos != null) && !listaLaudos.isEmpty())
+		// {
+		// System.out.println("Lista Laudos OK. " + listaLaudos.size());
+		// }
+		// else
+		// {
+		// this.listarLaudos();
+		// }
+		// //
+		// JasperCompileManager.compileReportToFile("/reports/TONOMETRIA.jrxml",
+		// // "/reports/TONOMETRIA.jasper");
+		// }
+		// catch (Exception e)
+		// {
+		// e.printStackTrace();
+		// return com.opensymphony.xwork2.Action.ERROR;
+		// }
 
 		return com.opensymphony.xwork2.Action.SUCCESS;
 	}
@@ -157,17 +167,14 @@ public class AutenticacaoLaudoOnlineAction
 	    @Result(location = "/jsp/autenticacaoLaudoOnline.jsp", name = "error")})
 	public String autenticarPaciente ()
 	{
-		// final Long nro = this.getNroCadastroPaciente();
-		if (this.getNroCadastroPaciente() == null)
+		final Long cdPessoa = this.getNroCadastroPaciente();
+
+		// validar campos de entrada.
+		if (cdPessoa == null)
 		{
 			this.addFieldError("nroCadastroPaciente", "* Campo Obrigatório. (Usar apenas Números)");
 			return com.opensymphony.xwork2.Action.ERROR;
 		}
-		// else
-		// {
-		// this.setNroCadastroPaciente(nro);
-		// }
-
 		if (this.getDataNascimento() == null)
 		{
 			this.addFieldError("dataNascimento",
@@ -175,7 +182,45 @@ public class AutenticacaoLaudoOnlineAction
 			return com.opensymphony.xwork2.Action.ERROR;
 		}
 
-		this.listarLaudos();
+		try
+		{
+			this.setPessoaVo(DaoFactory.getInstance().getAcPessoaByMatricula(cdPessoa));
+			if ((this.getPessoaVo() == null) || (this.getPessoaVo().getDataNascimento() == null))
+			{
+				this.addFieldError("nroCadastroPaciente", "* Este Nº de Matricula está Incorreto!!");
+				return com.opensymphony.xwork2.Action.ERROR;
+			}
+			// pessoa localizada, validar a data nascimento.
+			if (this.getPessoaVo().getDataNascimento().compareTo(this.getDataNascimento()) == 0)
+			{
+				this.addFieldError(
+				    "dataNascimento",
+				    "Data de Nascimento Incorreta para a Matrícula Informada!! Dt.Nasc. Matr: "
+				        + (DateUtils.formatDateDDMMYYYY(this.getPessoaVo().getDataNascimento())
+				            + " Dt.Nasc. Informada: " + DateUtils.formatDate(this.getDataNascimento())));
+				return com.opensymphony.xwork2.Action.ERROR;
+			}
+
+			System.out.println("pessoa.dtnasc: " + this.getPessoaVo().getDataNascimento());
+			System.out.println("dtnasc field: " + this.getDataNascimento());
+
+			// Pessoa localizada e data nascimento conferida. pesquisar os
+			// laudos.
+			this.listarLaudos();
+		}
+		catch (RuntimeException ex)
+		{
+			ex.printStackTrace();
+			this.addFieldError("nroCadastroPaciente", ex.getMessage());
+			return com.opensymphony.xwork2.Action.ERROR;
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+			this.addFieldError("nroCadastroPaciente", ex.getMessage());
+			return com.opensymphony.xwork2.Action.ERROR;
+		}
+
 		return com.opensymphony.xwork2.Action.SUCCESS;
 	}
 
@@ -187,55 +232,55 @@ public class AutenticacaoLaudoOnlineAction
 	{
 		List<LaudoVO> list = new ArrayList<LaudoVO>();
 		LaudoVO l1 = new LaudoVO();
-		l1.setId(1098L);
-		l1.setDescricao("LAUDO DE TONOMETRIA");
-		l1.setDataFinalizacao(Calendar.getInstance().getTime());
-		l1.setFinalizado(true);
-		list.add(l1);
+		// l1.setId(1098L);
+		// l1.setDescricao("LAUDO DE TONOMETRIA");
+		// l1.setDataFinalizacao(Calendar.getInstance().getTime());
+		// l1.setFinalizado(true);
+		// list.add(l1);
+		//
+		// LaudoVO l2 = new LaudoVO();
+		// l2.setId(3356L);
+		// l2.setDescricao("LAUDO DE ACUIDADE VISUAL (PAM)");
+		// l2.setDataFinalizacao(Calendar.getInstance().getTime());
+		// l2.setFinalizado(true);
+		// list.add(l2);
+		//
+		// LaudoVO l3 = new LaudoVO();
+		// l3.setId(1584L);
+		// l3.setDescricao("LAUDO DE BIOMETRIA ULTRASSONICA");
+		// l3.setDataFinalizacao(Calendar.getInstance().getTime());
+		// l3.setFinalizado(false);
+		// list.add(l3);
+		//
+		// LaudoVO l4 = new LaudoVO();
+		// l4.setId(1123L);
+		// l4.setDescricao("LAUDO DE MICROSCOPIA ESPECULAR DE CORNEA");
+		// l4.setDataFinalizacao(Calendar.getInstance().getTime());
+		// l4.setFinalizado(false);
+		// list.add(l4);
+		//
+		// LaudoVO L5 = new LaudoVO();
+		// L5.setId(7454L);
+		// L5.setDescricao("LAUDO DE CERATOSCOPIA COMPUTADORIZADA");
+		// L5.setDataFinalizacao(Calendar.getInstance().getTime());
+		// L5.setFinalizado(true);
+		// list.add(L5);
+		//
+		// LaudoVO L6 = new LaudoVO();
+		// L6.setId(9652L);
+		// L6.setDescricao("LAUDO DE MAPEAMENTO DE RETINA");
+		// L6.setDataFinalizacao(Calendar.getInstance().getTime());
+		// L6.setFinalizado(false);
+		// list.add(L6);
+		//
+		// LaudoVO L7 = new LaudoVO();
+		// L7.setId(3625L);
+		// L7.setDescricao("LAUDO DE RETINOGRAFIA FLUORESCENTE");
+		// L7.setDataFinalizacao(Calendar.getInstance().getTime());
+		// L7.setFinalizado(true);
+		// list.add(L7);
 
-		LaudoVO l2 = new LaudoVO();
-		l2.setId(3356L);
-		l2.setDescricao("LAUDO DE ACUIDADE VISUAL (PAM)");
-		l2.setDataFinalizacao(Calendar.getInstance().getTime());
-		l2.setFinalizado(true);
-		list.add(l2);
-
-		LaudoVO l3 = new LaudoVO();
-		l3.setId(1584L);
-		l3.setDescricao("LAUDO DE BIOMETRIA ULTRASSONICA");
-		l3.setDataFinalizacao(Calendar.getInstance().getTime());
-		l3.setFinalizado(false);
-		list.add(l3);
-
-		LaudoVO l4 = new LaudoVO();
-		l4.setId(1123L);
-		l4.setDescricao("LAUDO DE MICROSCOPIA ESPECULAR DE CORNEA");
-		l4.setDataFinalizacao(Calendar.getInstance().getTime());
-		l4.setFinalizado(false);
-		list.add(l4);
-
-		LaudoVO L5 = new LaudoVO();
-		L5.setId(7454L);
-		L5.setDescricao("LAUDO DE CERATOSCOPIA COMPUTADORIZADA");
-		L5.setDataFinalizacao(Calendar.getInstance().getTime());
-		L5.setFinalizado(true);
-		list.add(L5);
-
-		LaudoVO L6 = new LaudoVO();
-		L6.setId(9652L);
-		L6.setDescricao("LAUDO DE MAPEAMENTO DE RETINA");
-		L6.setDataFinalizacao(Calendar.getInstance().getTime());
-		L6.setFinalizado(false);
-		list.add(L6);
-
-		LaudoVO L7 = new LaudoVO();
-		L7.setId(3625L);
-		L7.setDescricao("LAUDO DE RETINOGRAFIA FLUORESCENTE");
-		L7.setDataFinalizacao(Calendar.getInstance().getTime());
-		L7.setFinalizado(true);
-		list.add(L7);
-
-		l1 = l2 = l3 = l4 = L5 = L6 = null;
+		// l1 = l2 = l3 = l4 = L5 = L6 = null;
 		return list;
 	}
 
@@ -244,7 +289,7 @@ public class AutenticacaoLaudoOnlineAction
 	 */
 	public List<LaudoVO> getListaLaudos ()
 	{
-		return listaLaudos;
+		return this.getPessoaVo() != null ? this.getPessoaVo().getListaLaudos() : null;
 	}
 
 	/**
@@ -252,7 +297,10 @@ public class AutenticacaoLaudoOnlineAction
 	 */
 	public void setListaLaudos (List<LaudoVO> listaLaudos)
 	{
-		this.listaLaudos = listaLaudos;
+		if (this.getPessoaVo() != null)
+		{
+			this.getPessoaVo().setListaLaudos(listaLaudos);
+		}
 	}
 
 	/**
@@ -286,4 +334,21 @@ public class AutenticacaoLaudoOnlineAction
 	{
 		this.dataNascimento = dataNascimento;
 	}
+
+	/**
+	 * @return the pessoaVo
+	 */
+	public PessoaVO getPessoaVo ()
+	{
+		return pessoaVo;
+	}
+
+	/**
+	 * @param pessoaVo the pessoaVo to set
+	 */
+	public void setPessoaVo (PessoaVO pessoaVo)
+	{
+		this.pessoaVo = pessoaVo;
+	}
+
 }
