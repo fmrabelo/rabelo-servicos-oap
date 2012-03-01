6503874fcee05e7711757b3352cb84c59670961e
fmrabelo <fmrabelo@38c0dbe2-1d61-b02a-7774-c78bf3521c0c>
2012-03-01 20:15:45 +0000

    implementando servlet e relatorio a partir de arquivo RTF formatado com e sem IText.

--- trunk/IntranetOAP/src/br/com/oappr/infra/action/AutenticacaoLaudoOnlineAction.java
+++ trunk/IntranetOAP/src/br/com/oappr/infra/action/AutenticacaoLaudoOnlineAction.java
@@ -3,11 +3,26 @@
  */
 package br.com.oappr.infra.action;
 
+import java.awt.Dialog;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.view.JasperViewer;
+import net.sourceforge.rtf.RTFTemplate;
+import net.sourceforge.rtf.helper.RTFTemplateBuilder;
 
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Result;
@@ -17,6 +32,15 @@ import br.com.oappr.infra.util.DateUtils;
 import br.com.oappr.intranet.vo.LaudoVO;
 import br.com.oappr.intranet.vo.PessoaVO;
 
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Font;
+import com.lowagie.text.HeaderFooter;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfWriter;
+import com.lowagie.text.rtf.RtfWriter2;
+import com.lowagie.text.rtf.parser.RtfParser;
 import com.opensymphony.xwork2.ActionSupport;
 
 /**
@@ -37,7 +61,7 @@ public class AutenticacaoLaudoOnlineAction
 	private PessoaVO pessoaVo;
 
 	// parametros para jasper report
-	HashMap<String, String> parameters = new HashMap<String, String>();
+	HashMap<String, Object> parameters = new HashMap<String, Object>();
 
 	// numero do cadastro do paciente
 	private Long nroCadastroPaciente;
@@ -48,7 +72,7 @@ public class AutenticacaoLaudoOnlineAction
 	/**
 	 * @return the parameters
 	 */
-	public HashMap<String, String> getParameters ()
+	public HashMap<String, Object> getParameters ()
 	{
 		return parameters;
 	}
@@ -56,7 +80,7 @@ public class AutenticacaoLaudoOnlineAction
 	/**
 	 * @param parameters the parameters to set
 	 */
-	public void setParameters (HashMap<String, String> parameters)
+	public void setParameters (HashMap<String, Object> parameters)
 	{
 		this.parameters = parameters;
 	}
@@ -93,12 +117,6 @@ public class AutenticacaoLaudoOnlineAction
 				for (LaudoVO v : this.getListaLaudos())
 				{
 					System.out.printf("ID: %s Laudo: %s %n", v.getNrlaudo(), v.getDsexamecompl());
-					System.out.println(" laudo: ");
-					long blobLength = v.getDsrtf().length();
-					final byte[] bt = v.getDsrtf().getBytes(1, (int)blobLength);
-					final String text = new String(bt);
-					System.out.println(text);
-
 				}
 			}
 			else
@@ -115,6 +133,215 @@ public class AutenticacaoLaudoOnlineAction
 	}
 
 	/**
+	 * @param filename
+	 * @param blob
+	 * @return
+	 */
+	public static boolean blobToFile2 (String filename, Blob blob)
+	{
+		boolean success = true;
+		try
+		{
+			// Get as a BLOB and put in a byte array.
+			byte[] allBytesInBlob = blob.getBytes(1, (int)blob.length());
+			final FileOutputStream outStream = new FileOutputStream(new File(filename));
+			outStream.write(allBytesInBlob);
+			outStream.flush();
+			outStream.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			System.out.println("ERROR(djv_exportBlob) Unable to export:" + filename);
+			success = false;
+		}
+		finally
+		{
+			return success;
+		}
+	}
+
+	/**
+	 * @param filename
+	 * @param blob
+	 * @return
+	 */
+	public static boolean blobToFile (String filename, Blob blob)
+	{
+		boolean success = true;
+		try
+		{
+			final FileOutputStream outStream = new FileOutputStream(new File(filename));
+			byte[] blobBuffer = new byte[(int)blob.length()];
+			final InputStream inStream = blob.getBinaryStream();
+			int length = -1;
+			while ((length = inStream.read(blobBuffer)) != -1)
+			{
+				outStream.write(blobBuffer, 0, length);
+				outStream.flush();
+			}
+			inStream.close();
+			outStream.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			System.out.println("ERROR(djv_exportBlob) Unable to export:" + filename);
+			success = false;
+		}
+		finally
+		{
+			return success;
+		}
+	}
+
+	/**
+	 * @param lista
+	 * @param caminhoRel
+	 * @throws Exception
+	 */
+	public void gerarRelPdf (List lista, String caminhoRel, String caminhoLogo) throws Exception
+	{
+		try
+		{
+			// passar o rtf como relatorio....
+			caminhoRel = "C:\\downloads\\text_375688.rtf";
+
+			// InputStream relatorio =
+			// this.getClass().getResourceAsStream(caminhoRel);
+			HashMap<String, Object> parametros = new HashMap<String, Object>();
+			parametros.put("LOGO", caminhoLogo);
+			// JRBeanCollectionDataSource colection = new
+			// JRBeanCollectionDataSource(lista);
+			// JREmptyDataSource colection = new JREmptyDataSource();
+			// JasperPrint impressao = JasperFillManager.fillReport(relatorio,
+			// parametros, colection);
+			JasperPrint impressao = JasperFillManager.fillReport(caminhoRel, parametros);
+			JasperExportManager.exportReportToPdf(impressao);
+
+			// JasperReport report = JasperCompileManager.compileReport(jrxml);
+			// JasperPrint impressao = JasperFillManager.fillReport(report,
+			// parameters);
+
+			final JasperViewer jv = new JasperViewer(impressao, false);
+			jv.setVisible(true);
+			jv.setModalExclusionType(Dialog.ModalExclusionType.APPLICATION_EXCLUDE);
+
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+			// JOptionPane.showMessageDialog(null, "Não foi possivel abrir o
+			// relatório");
+		}
+	}
+
+	public static void itextTemplate ()
+	{
+		String rtfSource = "C:\\downloads\\text_rtf.rtf";
+		String rtfTarget = "C:\\downloads\\DocumentoFinal.rtf";
+		try
+		{
+			// 1. Get default RTFtemplateBuilder
+			RTFTemplateBuilder builder = RTFTemplateBuilder.newRTFTemplateBuilder();
+
+			// 2. Get RTFtemplate with default Implementation of template engine
+			// (Velocity)
+			RTFTemplate rtfTemplate = builder.newRTFTemplate();
+
+			// 3. Set the RTF model source
+			rtfTemplate.setTemplate(new File(rtfSource));
+
+			// 4. AKI ALTERA AS VARIAVEIS
+			rtfTemplate.put("project", "Jakarta Velocity project");
+
+			// 5. AKI GRAVA O DOCUMENTO FINAL
+			rtfTemplate.merge(rtfTarget);
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	public static void addInRtf () throws Exception
+	{
+		// step 1: creation of a document-object
+		Document document = new Document();
+
+		// step 2:
+		// we create a writer that listens to the document
+		// and directs a RTF-stream to a file
+		RtfWriter2.getInstance(document, new FileOutputStream("C:\\downloads\\text_rtf.rtf"));
+		// step 3: we open the document
+		document.open();
+		// header
+		Phrase phrase = new Phrase("This is the first chapter");
+		HeaderFooter header1 = new HeaderFooter(phrase, true);
+		document.setHeader(header1);
+
+		Font titleFont = new Font(Font.TIMES_ROMAN, 14, Font.BOLD);
+		document.add(new Paragraph("Title1", titleFont));
+		// step 4: we add a paragraph to the document
+		Paragraph p = new Paragraph("Hello", titleFont);
+		String args[] = new String[]{
+		    "O exame de Microscopia Especular de Córnea foi realizado no Konan Specular Microscope Robo Pachy SP-9000.",
+		    "ANÁLISE MORFO-CELULARES",
+		    "- Predomínio de células hexagonais (6ª) em 00% na área analisada."};
+		for (int i = 0; i < args.length; i++)
+		{
+			p.add("\n");
+			p.add(args[i]);
+		}
+		document.add(p);
+
+		// step 5: we close the document
+		document.close();
+	}
+
+	/**
+	 * 
+	 */
+	public static void convertRTFToPDF ()
+	{
+		String inputFile = "C:\\downloads\\text_375688.rtf";
+		String outputFile = "C:\\downloads\\text_375688__itext.pdf";
+		// create a new document
+		Document document = new Document();
+		try
+		{
+			// create a PDF writer to save the new document to disk
+			PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(outputFile));
+			// open the document for modifications
+			document.open();
+			Font titleFont = new Font(Font.TIMES_ROMAN, 14, Font.BOLD);
+			document.add(new Paragraph("Title1", titleFont));
+			// create a new parser to load the RTF file
+			RtfParser parser = new RtfParser();
+			// read the rtf file into a compatible document
+			parser.convertRtfDocument(new FileInputStream(inputFile), document);
+			// save the pdf to disk
+			document.close();
+			System.out.println("Finished");
+		}
+		catch (DocumentException e)
+		{
+			e.printStackTrace();
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	/**
 	 * Gerar relatorio pdf.
 	 * @return
 	 */
@@ -122,86 +349,128 @@ public class AutenticacaoLaudoOnlineAction
 	{
 		System.out.println("Print PDF laudo especifico...");
 		// pesquisar laudos
-		// try
-		// {
-		// this.setListaLaudos(DaoFactory.getInstance().getLaudos(this.getNroCadastroPaciente(),
-		// this.getNroLaudo()));
-		// System.out.println("listar laudo. Quantidade-> "
-		// + (this.getListaLaudos() != null ? this.getListaLaudos().size() :
-		// 0));
-		// if ((this.getListaLaudos() != null) &&
-		// !this.getListaLaudos().isEmpty())
-		// {
-		// for (LaudoVO v : this.getListaLaudos())
-		// {
-		// System.out.printf("ID: %s Laudo: %s %n", v.getNrlaudo(),
-		// v.getDsexamecompl());
-		// }
-		// }
-		// else
-		// {
-		// this.addFieldError("nroCadastroPaciente", "Nenhum registro de Laudo
-		// localizado!!");
-		// return com.opensymphony.xwork2.Action.ERROR;
-		// }
-		// }
-		// catch (Exception ex)
-		// {
-		// ex.printStackTrace();
-		// }
+		try
+		{
+			Long nroCadastroPaciente2 = this.getNroCadastroPaciente();
+			if (nroCadastroPaciente2 == null)
+			{
+				nroCadastroPaciente2 = 114863L;
+			}
+			this.setListaLaudos(DaoFactory.getInstance().getLaudos(nroCadastroPaciente2,
+			    this.getNroLaudo()));
+			System.out.println("listar laudo. Quantidade-> "
+			    + (this.getListaLaudos() != null ? this.getListaLaudos().size() : 0));
+			if ((this.getListaLaudos() != null) && !this.getListaLaudos().isEmpty())
+			{
+				for (LaudoVO v : this.getListaLaudos())
+				{
+					System.out.printf("ID: %s Laudo: %s %n", v.getNrlaudo(), v.getDsexamecompl());
+				}
+			}
+			else
+			{
+				this.addFieldError("nroCadastroPaciente", "Nenhum registro de Laudo localizado!!");
+				return com.opensymphony.xwork2.Action.ERROR;
+			}
 
-		// HttpServletRequest request = ServletActionContext.getRequest();
-		// String imgPath = "";
-		//
-		// if (request != null)
-		// {
-		// imgPath = request.getSession().getServletContext().getRealPath(
-		// new
-		// StringBuilder(File.separator).append("logo").append(File.separator).append(
-		// "logo2.jpg").toString());
-		// }
-		// String id = request.getParameter("id");
+			// HttpServletRequest request = ServletActionContext.getRequest();
+			// String imgPath = "";
+			//
+			// if (request != null)
+			// {
+			// imgPath = request.getSession().getServletContext().getRealPath(
+			// new
+			// StringBuilder(File.separator).append("images").append(File.separator).append(
+			// "logo").append(File.separator).append("logo2.jpg").toString());
+			// }
+			// String id = request.getParameter("id");
 
-		// recuperar laudo pelo id
+			// recuperar laudo pelo id
 
-		// this.setListaLaudos(this.getLaudos());
-		// if ((listaLaudos != null) && !listaLaudos.isEmpty())
-		// {
-		// for (final LaudoVO v : listaLaudos)
-		// {
-		// if (v.getCdpessoa().longValue() == new Long(id).longValue())
-		// {
-		// laudo = v.getDescricao();
-		// break;
-		// }
-		// }
-		// }
-		//
-		// parameters.put("LOGO_PATH", imgPath);
-		// parameters.put("PACIENTE", "PACIENTE : " + paciente);
-		// parameters.put("DESCR_MEDICO", "A/C : " + medico);
-		// parameters.put("DESCR_LAUDO", laudo);
-		//
-		// this.setParameters(parameters);
-		// try
-		// {
-		// if ((listaLaudos != null) && !listaLaudos.isEmpty())
-		// {
-		// System.out.println("Lista Laudos OK. " + listaLaudos.size());
-		// }
-		// else
-		// {
-		// this.listarLaudos();
-		// }
-		// //
-		// JasperCompileManager.compileReportToFile("/reports/TONOMETRIA.jrxml",
-		// // "/reports/TONOMETRIA.jasper");
-		// }
-		// catch (Exception e)
-		// {
-		// e.printStackTrace();
-		// return com.opensymphony.xwork2.Action.ERROR;
-		// }
+			// this.setListaLaudos(this.getLaudos());
+			// if ((this.getListaLaudos() != null) &&
+			// !this.getListaLaudos().isEmpty())
+			// {
+			// for (final LaudoVO v : this.getListaLaudos())
+			// {
+			// if (v.getCdpessoa().longValue() == new Long(id).longValue())
+			// {
+			// // laudo = v.getDescricao();
+			// break;
+			// }
+			// }
+			// }
+
+			// if ((this.getListaLaudos() != null) &&
+			// !this.getListaLaudos().isEmpty())
+			// {
+			// System.out.println("Lista Laudos OK. " +
+			// this.getListaLaudos().size());
+			// }
+			// else
+			// {
+			// this.listarLaudos();
+			// }
+
+			// teste RTF to PDF
+			// convertRTFToPDF();
+			addInRtf();
+			//
+			// byte[] allBytesInBlob =
+			// this.getListaLaudos().get(0).getDsrtf().getBytes(1,
+			// (int)this.getListaLaudos().get(0).getDsrtf().length());
+			// System.out.println(new String(allBytesInBlob));
+			//
+			// // parameters.put("LAUDO_RTF", new String(allBytesInBlob));
+			//
+			// // parameters.put("LAUDO_RTF",
+			// // this.getListaLaudos().get(0).getDsrtf().getBinaryStream());
+			//
+			// parameters.put("LAUDO_RTF",
+			// "{\\rtf1\\ansi\\ansicpg1252\\deff0\\deflang1046{\\fonttbl{\\f0\fswiss\\fprq2\\fcharset0
+			// Arial;}}");
+			//
+			// parameters.put("LOGO_PATH", imgPath);
+			// parameters.put("PACIENTE", "PACIENTE : " +
+			// this.getPessoaVo().getNomePessoa());
+			// parameters.put("DESCR_MEDICO", "A/C : " + " XING LING PING ");
+			// parameters.put("DESCR_LAUDO",
+			// this.getListaLaudos().get(0).getDsexamecompl());
+			// this.setParameters(parameters);
+			//
+			// String jrxml =
+			// request.getSession().getServletContext().getRealPath(
+			// new
+			// StringBuilder(File.separator).append("reports").append(File.separator).append(
+			// "TONOMETRIA").append(".jrxml").toString());
+			//
+			// String jasper =
+			// request.getSession().getServletContext().getRealPath(
+			// new
+			// StringBuilder(File.separator).append("reports").append(File.separator).append(
+			// "TONOMETRIA").append(".jasper").toString());
+
+			// abrir relatorio
+			// JasperCompileManager.compileReportToFile(jrxml, jasper);
+
+			// this.gerarRelPdf(new ArrayList<Object>(), jrxml, imgPath);
+
+			// JasperReport report = JasperCompileManager.compileReport(jrxml);
+			// JasperPrint impressao = JasperFillManager.fillReport(report,
+			// parameters);
+			//
+			// JRPdfExporter pdf = new JRPdfExporter();
+			// pdf.setParameter(JRExporterParameter.JASPER_PRINT, impressao);
+			// pdf.setParameter(JRExporterParameter.INPUT_STREAM,
+			// this.getListaLaudos().get(0).getDsrtf().getBinaryStream());
+			// pdf.exportReport();
+
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return com.opensymphony.xwork2.Action.ERROR;
+		}
 
 		return com.opensymphony.xwork2.Action.SUCCESS;
 	}
@@ -350,6 +619,11 @@ public class AutenticacaoLaudoOnlineAction
 	{
 		if (this.getPessoaVo() != null)
 		{
+			this.getPessoaVo().setListaLaudos(listaLaudos);
+		}
+		else
+		{
+			this.setPessoaVo(new PessoaVO());
 			this.getPessoaVo().setListaLaudos(listaLaudos);
 		}
 	}
