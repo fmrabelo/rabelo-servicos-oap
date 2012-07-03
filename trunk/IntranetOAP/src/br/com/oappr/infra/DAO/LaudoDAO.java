81d3954d3c7407dbd4699d73cf6fae270bdf1516
fmrabelo <fmrabelo@38c0dbe2-1d61-b02a-7774-c78bf3521c0c>
2012-07-03 01:16:01 +0000

    OAP - alteração query para retorno de BLOB para coluna IMAGEM da nova tabela ACREQUISIMAGEM.

--- trunk/IntranetOAP/src/br/com/oappr/infra/DAO/LaudoDAO.java
+++ trunk/IntranetOAP/src/br/com/oappr/infra/DAO/LaudoDAO.java
@@ -59,28 +59,27 @@ final class LaudoDAO
 			conn = DaoFactory.getInstance().getConection();
 			if (conn != null)
 			{
-
 				stm = conn.createStatement();
 				str.append(" SELECT T2.NRREQUISICAO, T1.NRSEQRESULTADO, T1.DTCONSULTA, T1.HRAGENDA,T1.NRUSUARIOAMB, ");
 				str.append(" 	T1.CDPESSOA, T2.CDPROCED, T2.DSEXAMECOMPL, T2.NRLAUDO, T2.NRUSUARIOINC, T2.DHINCLUSAO, ");
-				str.append(" 	T2.NRUSUARIOALT, T2.DHALTERACAO, T1.CDCONVENIO,T2.NRUSUSOLIC, T2.DSRTF ");
-				str.append(" FROM SYSADM.AARESULTADO T1, SYSADM.ACREQUISICAO T2 ");
+				str.append(" 	T2.NRUSUARIOALT, T2.DHALTERACAO, T1.CDCONVENIO,T2.NRUSUSOLIC, T2.DSRTF, T3.IMAGEM ");
+				str.append(" FROM SYSADM.AARESULTADO T1, SYSADM.ACREQUISICAO T2, SYSADM.ACREQUISIMAGEM T3 ");
 				str.append("  WHERE T1.NRSEQRESULTADO = T2.NRSEQRESULTADO ");
 				str.append(" 	 AND T1.TPPRESENCA = 5 ");
+				str.append(" 	 AND T1.CDPESSOA = ").append(nroCadastroPaciente);// paciente
+				str.append(" 	 AND T2.TPLAUDO = 2 ");// 2=Laudo tipo RTF.
+				str.append(" 	 AND T2.FLLIBERADO = 1 ");// laudo liberado
 				str.append(" 	 AND T2.NRSEQRESULTADO IS NOT NULL ");
+				str.append(" 	 AND T2.NRREQUISICAO = T3.NRREQUISICAO ");
+				str.append(" 	 AND T2.CDPROCED = T3.CDPROCED ");
 				if ((codigoLaudo != null) && (codigoLaudo.longValue() > 0))
 				{
 					// --pesquisar laudo específico
 					str.append(" AND T2.NRSEQRESULTADO = ").append(codigoLaudo);
 					str.append(" AND T2.NRREQUISICAO = ").append(nroRequisicao);
 				}
-				str.append(" 	 AND T2.TPLAUDO = 2 ");// 2=Laudo tipo RTF.
-				str.append(" 	 AND T2.FLLIBERADO = 1 ");// laudo liberado
-				str.append(" 	 AND T1.CDPESSOA = ").append(nroCadastroPaciente);// paciente
 				str.append(" ORDER BY T1.DTCONSULTA DESC ");
-
 				rs = stm.executeQuery(str.toString());
-
 				lista = new ArrayList<LaudoVO>();
 				while ((rs != null) && rs.next())
 				{
@@ -105,10 +104,11 @@ final class LaudoDAO
 					p.setDtconsulta(rs.getDate("DTCONSULTA"));
 					p.setHragenda(rs.getDate("HRAGENDA"));
 					p.setDhalteracao(rs.getDate("DHALTERACAO"));
-					// blob para o laudo especifico.
+					// blob para o laudo especifico e imagens.
 					if ((codigoLaudo != null) && (codigoLaudo.longValue() > 0))
 					{
 						p.setDsrtf(rs.getBlob("DSRTF"));
+						p.setImages(rs.getBlob("IMAGEM"));
 					}
 					lista.add(p);
 				}
