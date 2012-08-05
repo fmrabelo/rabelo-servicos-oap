7c7a1bb030b98fda6905f5834c05f2ba2017773a
fmrabelo <fmrabelo@38c0dbe2-1d61-b02a-7774-c78bf3521c0c>
2012-08-05 00:45:57 +0000

    Teste para gravar e ler imagem BLOB oracle e gerar arquivo em disco.

--- IntranetOAP/src/br/com/oappr/conversor/ConvertPDF.java
+++ IntranetOAP/src/br/com/oappr/conversor/ConvertPDF.java
@@ -3,10 +3,14 @@
  */
 package br.com.oappr.conversor;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.Serializable;
 
 import br.com.oappr.conversor.office.ConversorFromTextOffice;
 import br.com.oappr.conversor.pdf.ConversorPDF;
+
+import com.lowagie.text.Document;
 
 /**
  * Classe responsável por converter conteudo RTF em PDF.
@@ -24,7 +28,7 @@ public final class ConvertPDF
 	 * @param arquivoRTF
 	 * @return byte[]
 	 */
-	public final byte[] convertPDF (final byte[] arquivoRTF) throws Exception
+	public final byte[] convertRTF_To_PDF (final byte[] arquivoRTF) throws Exception
 	{
 		try
 		{
@@ -37,5 +41,45 @@ public final class ConvertPDF
 			ex.printStackTrace();
 			throw ex;
 		}
+	}
+
+	/**
+	 * @param jpg
+	 * @return byte[]
+	 */
+	public final byte[] convertJPG_To_PDF (final byte[] jpg) throws Exception
+	{
+		final Document document = new Document();
+		// String input = "c://temp//MOB.jpg"; // .gif and .jpg are ok too!
+		final String output = "c://temp//vicenteDestro.jpg";
+		try
+		{
+			final FileOutputStream fos = new FileOutputStream(new File(output));
+			fos.write(jpg);
+			fos.flush();
+			fos.close();
+
+			// final PdfWriter writer = PdfWriter.getInstance(document, fos);
+			// writer.open();
+			// document.open();
+			// // document.add(Image.getInstance(jpg));
+			// final Image instance = Image.getInstance(input);
+			// instance.scalePercent(30);
+			// document.add(instance);
+			// document.close();
+			// writer.close();
+
+			// ------------------------------------------
+			/** se precisar tratar tamanho da imagem */
+			// img.scalePercent(50);
+			// document.add(img);
+			// ------------------------------------------
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+			throw ex;
+		}
+		return jpg;
 	}
 }
