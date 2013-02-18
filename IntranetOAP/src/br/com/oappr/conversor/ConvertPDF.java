af14f2fc28447d3f6427ecabddf8812a2d086706
fmrabelo <fmrabelo@38c0dbe2-1d61-b02a-7774-c78bf3521c0c>
2013-02-18 19:34:20 +0000

    Inserção de regras para zoom em algumas imagens.

--- IntranetOAP/src/br/com/oappr/conversor/ConvertPDF.java
+++ IntranetOAP/src/br/com/oappr/conversor/ConvertPDF.java
@@ -13,6 +13,8 @@ import org.apache.commons.io.IOUtils;
 
 import br.com.oappr.conversor.office.ConversorFromTextOffice;
 import br.com.oappr.conversor.pdf.ConversorPDF;
+import br.com.oappr.infra.action.servlet.ServletReport;
+import br.com.oappr.infra.util.GenericUtils;
 
 import com.lowagie.text.Document;
 import com.lowagie.text.Image;
@@ -53,7 +55,8 @@ public final class ConvertPDF
 	 * @param jpg
 	 * @return byte[]
 	 */
-	public final byte[] convertJPG_To_PDF (final Blob imageBlob) throws Exception
+	public final byte[] convertJPG_To_PDF (final Blob imageBlob, final String nomeExame)
+	    throws Exception
 	{
 		final Document document = new Document();
 		// String input = "c://temp//MOB.jpg"; // .gif and .jpg are ok too!
@@ -74,11 +77,21 @@ public final class ConvertPDF
 			final byte[] byteArrayImg = imageBlob.getBytes(1, (int)imageBlob.length());
 			final Image instanceImg = Image.getInstance(byteArrayImg);
 			// ------------------------------------------
-			/** se precisar tratar tamanho da imagem */
+			/** se precisar tratar zoom da imagem */
 			// img.scalePercent(50);
+			if (GenericUtils.verificarNomeExames(nomeExame, ServletReport.EXAMES_MENOR_ZOOM))
+			{
+				// usar zoom 30 com imagem exportada com o Driver
+				instanceImg.scalePercent(30);
+			}
+			else
+			{
+				// usar zoom 57 com imagem exportada com clinitolls EXPORT.
+				instanceImg.scalePercent(57);
+			}
 			// ------------------------------------------
-			instanceImg.scalePercent(30);
 			document.add(instanceImg);
+			// document.setMargins(180, 108, 72, 36);
 			document.close();
 			pdfWriter.close();
 			fos.close();
