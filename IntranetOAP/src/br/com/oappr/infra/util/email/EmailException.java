3ec28e087af3897476e154daca37b121d2c79493
fmrabelo <fmrabelo@38c0dbe2-1d61-b02a-7774-c78bf3521c0c>
2013-03-29 22:36:31 +0000

    Implementação de novas regras e códigos para alteração de senha e lembrar senha esquecida.

---
+++ IntranetOAP/src/br/com/oappr/infra/util/email/EmailException.java
@@ -0,0 +1,55 @@
+package br.com.oappr.infra.util.email;
+
+import java.io.Serializable;
+
+/**
+ * <ul>
+ * A classe EmailException disponibiliza métodos responsáveis por gerar uma
+ * exceção referente a email.
+ * </ul>
+ * @author Desenvolvimento
+ */
+public final class EmailException
+    extends Exception
+    implements Serializable
+{
+
+	/**
+	 * Número serial da classe.
+	 */
+	private static final long serialVersionUID = -5517799350867844155L;
+
+	/**
+	 * Construtor da classe EmailException sem parametro.
+	 */
+	public EmailException ()
+	{
+		super();
+	}
+
+	/**
+	 * Construtor da classe EmailException com o parametro msg
+	 * @param msg String Mensagem para a exceção
+	 */
+	public EmailException (String msg)
+	{
+		super(msg);
+	}
+
+	/**
+	 * @param exception
+	 */
+	public EmailException (Exception exception)
+	{
+		super(exception);
+	}
+
+	/**
+	 * @param exception
+	 * @param msg
+	 */
+	public EmailException (Exception exception, String msg)
+	{
+		super(msg, exception);
+	}
+}
