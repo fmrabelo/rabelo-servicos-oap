3e5fede659ada47d0a9cc6606ce9049809ebf43c
fmrabelo <fmrabelo@38c0dbe2-1d61-b02a-7774-c78bf3521c0c>
2015-04-10 00:08:59 +0000

    selecao de arquivos por paciente

--- trunk/IntranetLaserView/src/br/com/laserviewpr/infra/action/LaudoOnlineAction.java
+++ trunk/IntranetLaserView/src/br/com/laserviewpr/infra/action/LaudoOnlineAction.java
@@ -27,12 +27,11 @@ import com.opensymphony.xwork2.ActionSupport;
 
 /**
  * Classe responsável pela lógica de autenticação para acesso ao laudo onLine.
+ * 
  * @author Rabelo Serviços.
  */
-public class LaudoOnlineAction
-    extends ActionSupport
-    implements ReportParameters
-{
+public class LaudoOnlineAction extends ActionSupport implements
+		ReportParameters {
 	/**
 	 * 
 	 */
@@ -50,9 +49,8 @@ public class LaudoOnlineAction
 	 * 
 	 */
 	@Override
-	@Action(value = "autenticacaoLaudoOnline", results = {@Result(location = "/jsp/autenticacaoLaudoOnline.jsp", name = "success")})
-	public String execute ()
-	{
+	@Action(value = "autenticacaoLaudoOnline", results = { @Result(location = "/jsp/autenticacaoLaudoOnline.jsp", name = "success") })
+	public String execute() {
 		this.setDataNascimento(Calendar.getInstance().getTime());
 		return SUCCESS;
 	}
@@ -68,28 +66,27 @@ public class LaudoOnlineAction
 	 * Neste caso é preciso criar o objeto pessoa apartir do codigo do cadastro
 	 * passado como parametro para a pesquisa.
 	 * </p>
+	 * 
 	 * @return
 	 * @throws Exception
 	 */
 	@Action(value = "listarLaudos", results = {
-	    @Result(location = "/jsp/listaLaudos.jsp", name = "success"),
-	    @Result(location = "/jsp/listaLaudos.jsp", name = "error")})
-	public String listarLaudos () throws Exception
-	{
+			@Result(location = "/jsp/listaLaudos.jsp", name = "success"),
+			@Result(location = "/jsp/listaLaudos.jsp", name = "error") })
+	public String listarLaudos() throws Exception {
 		// pesquisar laudos
-		try
-		{
-			final UsuarioInternoVO us = LaudoColaboradoresAction.getUserSession();
+		try {
+			final UsuarioInternoVO us = LaudoColaboradoresAction
+					.getUserSession();
 			if ((this.getPessoaVo() == null) && (us != null)
-			    && (this.getNroCadastroPaciente() != null) && (this.getNroCadastroPaciente() > 0))
-			{
+					&& (this.getNroCadastroPaciente() != null)
+					&& (this.getNroCadastroPaciente() > 0)) {
 				// Laudos internos, pesquisar o paciente e atribuí-lo a pessoa
 				// do laudo.
-				this.setPessoaVo(DaoFactory.getInstance().getPacienteByCodMatricula(
-				    this.getNroCadastroPaciente()));
-			}
-			else
-			{
+				this.setPessoaVo(DaoFactory.getInstance()
+						.getPacienteByCodMatricula(
+								this.getNroCadastroPaciente()));
+			} else {
 				// setar valor para indicar que é pesquisa de laudo por
 				// paciente.
 				this.getPessoaVo().setUrlSite("is_url_site");
@@ -97,17 +94,15 @@ public class LaudoOnlineAction
 			// pesquisar laudos na base de dados
 			// this.setListaLaudos(DaoFactory.getInstance().getLaudos(this.getNroCadastroPaciente(),
 			// null, null));
-			
-			//imagens de laudos 
+
+			// imagens de laudos específicos do paciente logado no sistema.
 			this.setListaLaudos(this.lerArquivosImages());
-			if (!Validator.notEmptyCollection(this.getListaLaudos()))
-			{
-				this.addFieldError("nroCadastroPaciente", "Nenhum Laudo localizado!!");
+			if (!Validator.notEmptyCollection(this.getListaLaudos())) {
+				this.addFieldError("nroCadastroPaciente",
+						"Nenhum Laudo localizado!!");
 				return ERROR;
 			}
-		}
-		catch (Exception ex)
-		{
+		} catch (Exception ex) {
 			ex.printStackTrace();
 			throw new Exception(ex);
 		}
@@ -118,45 +113,47 @@ public class LaudoOnlineAction
 	 * @return
 	 */
 	@Action(value = "autenticarPaciente", results = {
-	    @Result(location = "/jsp/listaLaudos.jsp", name = "success"),
-	    @Result(location = "/jsp/autenticacaoLaudoOnline.jsp", name = "error")})
-	public String autenticarPaciente ()
-	{
+			@Result(location = "/jsp/listaLaudos.jsp", name = "success"),
+			@Result(location = "/jsp/autenticacaoLaudoOnline.jsp", name = "error") })
+	public String autenticarPaciente() {
 		final Long cdPessoa = this.getNroCadastroPaciente();
 
 		// validar campos de entrada.
-		if (cdPessoa == null)
-		{
-			this.addFieldError("nroCadastroPaciente", "* Campo Obrigatório. (Usar apenas Números)");
+		if (cdPessoa == null) {
+			this.addFieldError("nroCadastroPaciente",
+					"* Campo Obrigatório. (Usar apenas Números)");
 			return ERROR;
 		}
-		if (this.getDataNascimento() == null)
-		{
-			this.addFieldError("dataNascimento",
-			    "* Campo Obrigatório. Preencher com uma Data Válida no formato dd/mm/aaaa (Exemplo: 01/05/1989)");
+		if (this.getDataNascimento() == null) {
+			this
+					.addFieldError(
+							"dataNascimento",
+							"* Campo Obrigatório. Preencher com uma Data Válida no formato dd/mm/aaaa (Exemplo: 01/05/1989)");
 			return ERROR;
 		}
 
-		try
-		{
-			this.setPessoaVo(DaoFactory.getInstance().getPacienteByCodMatricula(cdPessoa));
-			if ((this.getPessoaVo() == null) || (this.getPessoaVo().getDataNascimento() == null))
-			{
-				this.addFieldError("nroCadastroPaciente", "* Este Nº de Matricula está Incorreto!!");
+		try {
+			this.setPessoaVo(DaoFactory.getInstance()
+					.getPacienteByCodMatricula(cdPessoa));
+			if ((this.getPessoaVo() == null)
+					|| (this.getPessoaVo().getDataNascimento() == null)) {
+				this.addFieldError("nroCadastroPaciente",
+						"* Este Nº de Matricula está Incorreto!!");
 				return ERROR;
 			}
 			// pessoa localizada, validar a data nascimento. Adicionar +1h nas
 			// datas para garantir distorção no horário de verão em algumas
 			// regioes.
-			final Date dataNascPessoa = DateUtils.addAmountDate(
-			    this.getPessoaVo().getDataNascimento(), +1, Calendar.HOUR_OF_DAY);
-			final Date dataNascField = DateUtils.addAmountDate(this.getDataNascimento(), +1,
-			    Calendar.HOUR_OF_DAY);
+			final Date dataNascPessoa = DateUtils.addAmountDate(this
+					.getPessoaVo().getDataNascimento(), +1,
+					Calendar.HOUR_OF_DAY);
+			final Date dataNascField = DateUtils.addAmountDate(this
+					.getDataNascimento(), +1, Calendar.HOUR_OF_DAY);
 
-			if (dataNascPessoa.compareTo(dataNascField) != 0)
-			{
-				this.addFieldError("dataNascimento",
-				    "Data de Nascimento Incorreta para a Matrícula Informada!!");
+			if (dataNascPessoa.compareTo(dataNascField) != 0) {
+				this
+						.addFieldError("dataNascimento",
+								"Data de Nascimento Incorreta para a Matrícula Informada!!");
 				// Dt.Nasc. Matr: " +
 				// (DateUtils.formatDateDDMMYYYY(dataNascPessoa) + " Dt.Nasc.
 				// Informada: " + DateUtils.formatDate(dataNascField)));
@@ -166,15 +163,11 @@ public class LaudoOnlineAction
 			// Pessoa localizada e data nascimento conferida. pesquisar os
 			// laudos.
 			this.listarLaudos();
-		}
-		catch (RuntimeException ex)
-		{
+		} catch (RuntimeException ex) {
 			ex.printStackTrace();
 			this.addFieldError("nroCadastroPaciente", ex.getMessage());
 			return ERROR;
-		}
-		catch (Exception ex)
-		{
+		} catch (Exception ex) {
 			ex.printStackTrace();
 			this.addFieldError("nroCadastroPaciente", ex.getMessage());
 			return ERROR;
@@ -186,18 +179,17 @@ public class LaudoOnlineAction
 	/**
 	 * @return the listaLaudos
 	 */
-	public List<LaudoVO> getListaLaudos ()
-	{
-		return this.getPessoaVo() != null ? this.getPessoaVo().getListaLaudos() : null;
+	public List<LaudoVO> getListaLaudos() {
+		return this.getPessoaVo() != null ? this.getPessoaVo().getListaLaudos()
+				: null;
 	}
 
 	/**
-	 * @param listaLaudos the listaLaudos to set
+	 * @param listaLaudos
+	 *            the listaLaudos to set
 	 */
-	public void setListaLaudos (List<LaudoVO> listaLaudos)
-	{
-		if (this.getPessoaVo() == null)
-		{
+	public void setListaLaudos(List<LaudoVO> listaLaudos) {
+		if (this.getPessoaVo() == null) {
 			this.setPessoaVo(new PessoaVO());
 		}
 		this.getPessoaVo().setListaLaudos(listaLaudos);
@@ -206,64 +198,60 @@ public class LaudoOnlineAction
 	/**
 	 * @return the nroCadastroPaciente
 	 */
-	public Long getNroCadastroPaciente ()
-	{
+	public Long getNroCadastroPaciente() {
 		return nroCadastroPaciente;
 	}
 
 	/**
-	 * @param nroCadastroPaciente the nroCadastroPaciente to set
+	 * @param nroCadastroPaciente
+	 *            the nroCadastroPaciente to set
 	 */
-	public void setNroCadastroPaciente (Long nroCadastroPaciente)
-	{
+	public void setNroCadastroPaciente(Long nroCadastroPaciente) {
 		this.nroCadastroPaciente = nroCadastroPaciente;
 	}
 
 	/**
 	 * @return the dataNascimento
 	 */
-	public Date getDataNascimento ()
-	{
+	public Date getDataNascimento() {
 		return dataNascimento;
 	}
 
 	/**
-	 * @param dataNascimento the dataNascimento to set
+	 * @param dataNascimento
+	 *            the dataNascimento to set
 	 */
-	public void setDataNascimento (Date dataNascimento)
-	{
+	public void setDataNascimento(Date dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
 
 	/**
 	 * @return the pessoaVo
 	 */
-	public PessoaVO getPessoaVo ()
-	{
+	public PessoaVO getPessoaVo() {
 		return pessoaVo;
 	}
 
 	/**
-	 * @param pessoaVo the pessoaVo to set
+	 * @param pessoaVo
+	 *            the pessoaVo to set
 	 */
-	public void setPessoaVo (PessoaVO pessoaVo)
-	{
+	public void setPessoaVo(PessoaVO pessoaVo) {
 		this.pessoaVo = pessoaVo;
 	}
 
 	/**
 	 * @return the nroLaudo
 	 */
-	public Long getNroLaudo ()
-	{
+	public Long getNroLaudo() {
 		return nroLaudo;
 	}
 
 	/**
-	 * @param nroLaudo the nroLaudo to set
+	 * @param nroLaudo
+	 *            the nroLaudo to set
 	 */
-	public void setNroLaudo (Long nroLaudo)
-	{
+	public void setNroLaudo(Long nroLaudo) {
 		this.nroLaudo = nroLaudo;
 	}
 
@@ -271,43 +259,50 @@ public class LaudoOnlineAction
 	 * Método responsavel por ler nomes dos arquivos de imagens para paciente
 	 * especifico.
 	 */
-	public List<LaudoVO> lerArquivosImages ()
-	{
+	public List<LaudoVO> lerArquivosImages() {
 		// C:\Documentos\Sistemas\Risc\Images
-		// Há 3 arquivos nesse diretorio: 11932_080415_1.pdf,
-		// 11932_080415_2.JPG, 11932_080415_2.pdf
+		// Há diversos arquivos para diversos pacientes nesse diretorio:
+		// 11932_080415_1.pdf, 11932_080415_2.JPG, 11932_080415_2.pdf
 
-		// String caminho = javax.swing.JOptionPane.showInputDialog("digite o
-		// diretorio a ser listado");
+		// retornar apenas arquivos com mesmo codigo do paciente logado.
+		final Long cdPessoa = this.getPessoaVo().getCdPessoa();
+		if (cdPessoa == null) {
+			return null;
+		}
+
 		String caminho = CarregarArquivoLocal.DIRETORIO_IMAGENS;
 		List<LaudoVO> lista = null;
+
+		// retorna conteúdo do diretorio.
 		File raiz = new File(caminho);
-		if (raiz.exists())
-		{
+		if (raiz.exists()) {
+
 			final StringBuilder sb = new StringBuilder();
 			Scanner scanner = null;
 			lista = new ArrayList<LaudoVO>();
 			LaudoVO p = null;
-			for (File f : raiz.listFiles())
-			{
-				if (f.isFile())
-				{
-					p = new LaudoVO();
+			Long codtemp = null;
+			for (File f : raiz.listFiles()) {
+				if (f.isFile()) {
 					// quebrar nome do arquivo usando expressao regular:
 					// 11932_080415_2.JPG
 					scanner = new Scanner(f.getName()).useDelimiter("_");
-					while (scanner.hasNext())
-					{
-						p.setCdconvenio(GenericUtils.toLong(scanner.next()));
-						p.setDsexamecompl(f.getName());
-						// p.setDtconsulta(GenericUtils.formatarString(scanner.next(),""));
-						scanner.next();// data
-						scanner.next();// sequencia + extensao
+					codtemp = GenericUtils.toLong(scanner.next());
+					if (codtemp != null
+							&& cdPessoa.longValue() == codtemp.longValue()) {
+						p = new LaudoVO();
+						while (scanner.hasNext()) {
+							p.setCdconvenio(codtemp);
+							p.setDsexamecompl(f.getName());// file name.
+							p.setDtconsulta(DateUtils.parseDate(DateUtils
+									.convertToMinDateDDMMYY(scanner.next())));// data
+							scanner.next();// sequencia + extensao
+						}
+						lista.add(p);
+						// log
+						sb.append(f.getName());
+						sb.append("\n");
 					}
-					lista.add(p);
-					// log
-					sb.append(f.getName());
-					sb.append("\n");
 				}
 			}
 			System.out.println(sb.toString());
